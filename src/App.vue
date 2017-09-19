a15a00668d2ccd8a6984cfb21fcebc0812a1708b
yanhua1117 <yh_rw828@163.com>
2017-09-19 18:03:11 +0800

    2017-09-19 18:03

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,22 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <!--<router-view></router-view>-->
+    <div class="header">
+    	i am header!
+    </div>
+    <div class="tab">
+    	i am tab
+    </div>
+    <div class="content">
+    	i am content
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
